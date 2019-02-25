617135592bd7e818f451dccc56221e4b11a781dd
Sayed Atif <sayedatif86@gmail.com>
2019-02-25 20:24:37 +0530

    accomodate score on cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,6 +8,7 @@ const Card = ({ item }) => (
 			<p class={style.detailText}><strong>Name:</strong> {item.givenName}</p>
 			<p class={style.detailText}><strong>Middle:</strong> {item.middleName}</p>
 			<p class={style.detailText}><strong>Surname:</strong> {item.surname}</p>
+			<p class={style.detailText}><strong>Score:</strong> {item.score}</p>
 		</div>
 	</div>
 );
